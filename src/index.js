805d799bedeed020f01a2585e6e2991e138e1129
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-04-16 23:54:25 +0500

    css_players file added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './css_folder/index.css';
 import './css_folder/css_section_mid.css';
 import './css_folder/css_color_section.css';
+import './css_folder/css_players.css';
 import App from './App';
 
 
